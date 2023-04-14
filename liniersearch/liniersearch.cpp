cc6281853812b29a667c62ea41f1da93e9e7db98
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-04-14 15:18:09 +0700

    memperbaikin syntax untuk banyak nya data dalam array

--- liniersearch/liniersearch.cpp
+++ liniersearch/liniersearch.cpp
@@ -21,46 +21,10 @@ void input()
 	cout << "\n-------------------\n";
 	cout << "Enter array elements \n";
 	cout << "---------------------\n";
-	for (i = 0; 1 < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		cout << "<" << (i + 1) << ">";
 		cin >> arr[i];
 	}
 }
 
-void LinearSearch()
-{
-	char ch;
-	int comparisons; //number of comparisons
-
-	do
-	{
-		//accept the number to be searched
-		cout << "\nEnter the element you want to search: ";  //langkah 1
-		int item;
-		cin >> item;
-
-		comparisons = 0;
-		for (i = 0; i < n; i++)  //langkah 2,3 dan 4 
-		{
-			comparisons++;
-			if (arr[i] == item) //langkah 5A found
-			{
-				cout << "\n" << item << "found at position" << (i + 1) << endl;
-				break;
-			}
-		}
-		if (i == n)   //langkah 5B not found
-			cout << "\n" << item << "not found in the array\n";
-		cout << "\nNumber od comparisons: " << comparisons << endl;
-
-		cout << "\nContinue search (y/n): ";
-		cin >> ch;
-	} while ((ch == 'y') || (ch == 'y'));
-}
-
-int main()
-{
-	input();
-	LinearSearch();
-}
